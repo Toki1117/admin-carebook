74f02bf5418ea5c03ca0f61399e54b79729a2489
dmsq <dmsq2017@outlook.com>
2021-03-13 03:00:04 -0600

    Adding imports and details in services

--- src/app/physical-activity/physical-activity.module.ts
+++ src/app/physical-activity/physical-activity.module.ts
@@ -4,14 +4,28 @@ import { PhysicalActivityService } from './services/physical-activity.service';
 import { FormPhysicalComponent } from './components/form-physical/form-physical.component';
 import { PhysicalActivityComponent } from './views/physical-activity/physical-activity.component';
 import { LayoutComponent } from './views/layout/layout.component';
-
-
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmDialogModule } from '../shared/confirm-dialog/confirm-dialog.module';
+import { FormBuilderHelperModule } from '../shared/form-builder-helper/form-builder-helper.module';
+import { FullTableModule } from '../shared/full-table/full-table.module';
+import { SidebarDLModule } from '../shared/sidebar-dl/sidebar.module';
+import { PhysicalActivityRoutingModule } from './physical-activity-rounting.module';
 
 @NgModule({
-  declarations: [FormPhysicalComponent, PhysicalActivityComponent, LayoutComponent],
-  imports: [
-    CommonModule
+  declarations: [
+    FormPhysicalComponent,
+    PhysicalActivityComponent,
+    LayoutComponent,
   ],
-  providers:[PhysicalActivityService]
+  imports: [
+    CommonModule,
+    PhysicalActivityRoutingModule,
+    FullTableModule,
+    SidebarDLModule,
+    NgbModalModule,
+    ConfirmDialogModule,
+    FormBuilderHelperModule,
+  ],
+  providers: [PhysicalActivityService],
 })
-export class PhysicalActivityModule { }
+export class PhysicalActivityModule {}
