c3360b8b86ff87cdbbe1d1eab5bd76456dec20ed
dmsq <dmsq2017@outlook.com>
2021-03-13 11:47:05 -0600

    CRUD addcitions

--- src/app/addiction-type/addiction-type-routing.module.ts
+++ src/app/addiction-type/addiction-type-routing.module.ts
@@ -14,7 +14,10 @@ const routes: Routes = [ {
       },
       {
         path: 'overview',
-        component: AddictionTypeComponent
+        component: AddictionTypeComponent,
+        data: {
+          title: 'Tipo de Adicciones'
+        }
       }
     ]
   }];
--- src/app/addiction-type/services/addiction-type.service.ts
+++ src/app/addiction-type/services/addiction-type.service.ts
@@ -2,32 +2,42 @@ import { Injectable } from '@angular/core';
 import { AddictionType } from '../models/addiction-type';
 import { HttpClient } from '@angular/common/http';
 import { API_URL } from 'src/app/shared/utils/api-urls';
-
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ApiResponse } from 'src/app/core/models/api-response.model';
 
 @Injectable()
 export class AddictionTypeService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAddictionTypes() {
-    return this.http.get<AddictionType>(API_URL.addictionType.main)
+    return this.http.get<AddictionType[]>(API_URL.addictionType.main);
   }
 
   getAddictionTypeById(id: string) {
-    return this.http.get<AddictionType>(API_URL.addictionType.byId(id))
+    return this.http.get<AddictionType>(API_URL.addictionType.byId(id));
   }
 
-  createAddictionType (addictionType: AddictionType) {
-    const {name} = addictionType
-    return this.http.post<AddictionType>(API_URL.addictionType.create, name)
+  createAddictionType(
+    addictionType: AddictionType
+  ): Observable<ApiResponse<AddictionType>> {
+    return this.http
+      .post<AddictionType>(API_URL.addictionType.create, addictionType)
+      .pipe(map((data) => ({ data, message: 'Guardado exitoso' })));
   }
 
-  updateAddictionType (addictionType: AddictionType, id: string) {
-    const {name} = addictionType
-    return this.http.put<AddictionType>(API_URL.addictionType.update(id), name)
+  updateAddictionType(
+    addictionType: AddictionType,
+    id: string
+  ): Observable<ApiResponse<AddictionType>> {
+    return this.http
+      .put<AddictionType>(API_URL.addictionType.update(id), addictionType)
+      .pipe(map((data) => ({ data, message: 'Actualización exitosa' })));
   }
 
-  deleteAddictionType (id: string) {
-    return this.http.delete<AddictionType>(API_URL.addictionType.delete(id))
+  deleteAddictionType(id: string): Observable<ApiResponse<any>> {
+    return this.http
+      .delete<AddictionType>(API_URL.addictionType.delete(id))
+      .pipe(map((data) => ({ data, message: 'Borrado exitoso' })));
   }
 }
